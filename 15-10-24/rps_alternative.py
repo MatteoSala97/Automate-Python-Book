ebb747290833528e69e23e4da03e9b2375a79c78
MatteoSala97 <matteo111997@gmail.com>
2024-10-15 17:56:11 +0200

    rps alternative using structural pattern matching

---
+++ 15-10-24/rps_alternative.py
@@ -0,0 +1,71 @@
+import random
+import sys
+
+print("LET'S PLAY ROCK PAPER SCISSORS!")
+
+# Storing data about wins, losses, and ties
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    print(f"Wins: {wins} - Losses: {losses} - Ties: {ties}")
+    
+    while True:
+        print("Enter your move: (r)ock, (p)aper, (s)cissors or press (q) to quit")
+        player_move = input().lower()  
+        
+        match player_move:
+            case 'q':
+                print('Shutting down...')
+                sys.exit()
+            case 'r' | 'p' | 's':
+                break
+            case _:
+                print('Type one between "r", "p", "s" or "q"')
+    
+    # Display player's move
+    match player_move:
+        case 'r':
+            print('ROCK versus...')
+        case 'p':
+            print('PAPER versus...')
+        case 's':
+            print('SCISSORS versus...')
+    
+    # Display computer's mvoe       
+    computer_move = random.choice(['r', 'p', 's'])
+    match computer_move:
+        case 'r':
+            print('ROCK')
+        case 'p':
+            print('PAPER')
+        case 's':
+            print('SCISSORS')
+    
+    # updates scoreboard
+    if player_move == computer_move:
+        print('It is a tie!')
+        ties += 1
+    elif (player_move == 'r' and computer_move == 's') or \
+         (player_move == 'p' and computer_move == 'r') or \
+         (player_move == 's' and computer_move == 'p'):
+        print('You win!')
+        wins += 1
+    else:
+        print('You lose!')
+        losses += 1 
+
+            
+            
+# parser = argparse.ArgumentParser()
+# parser.add_argument('command', choices=['push', 'pull', 'commit'])
+# args = parser.parse_args()
+
+# match args.command:
+#     case 'push':
+#         print('pushing')
+#     case 'pull':
+#         print('pulling')
+#     case _:
+#         parser.error(f'{args.command!r} not yet implemented')
